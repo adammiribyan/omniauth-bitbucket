66ab94ce2ed82c7fe7b9ac0a1f9f58137f523682
Adam Miribyan <a.miribyan@mozestudio.com>
2014-11-25 11:17:50 +0100

    Tidy it up.

--- lib/omniauth/strategies/bitbucket.rb
+++ lib/omniauth/strategies/bitbucket.rb
@@ -3,31 +3,24 @@ require 'omniauth-oauth'
 module OmniAuth
   module Strategies
     class Bitbucket < OmniAuth::Strategies::OAuth
-      # This is where you pass the options you would pass when
-      # initializing your consumer from the OAuth gem.
       option :client_options, {
-        :site => 'https://bitbucket.org',
-        :request_token_path => '/api/1.0/oauth/request_token',
-        :authorize_path     => '/api/1.0/oauth/authenticate',
-        :access_token_path  => '/api/1.0/oauth/access_token'
+        site: 'https://bitbucket.org',
+        request_token_path: '/api/1.0/oauth/request_token',
+        authorize_path: '/api/1.0/oauth/authenticate',
+        access_token_path: '/api/1.0/oauth/access_token'
       }
 
-      # These are called after authentication has succeeded. If
-      # possible, you should try to set the UID without making
-      # additional calls (if the user id is returned with the token
-      # or as a URI parameter). This may not be possible with all
-      # providers.
       uid { raw_info['username'] }
 
       extra do
-        { :raw_info => raw_info }
+        { raw_info: raw_info }
       end
 
       info do
         {
-          :name => "#{raw_info['first_name']} #{raw_info['last_name']}",
-          :avatar => raw_info['avatar'],
-          :email => raw_info['email']
+          name: "#{raw_info['first_name']} #{raw_info['last_name']}",
+          avatar: raw_info['avatar'],
+          email: raw_info['email']
         }
       end
 
